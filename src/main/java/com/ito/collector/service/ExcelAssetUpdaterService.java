b2df2eb7911e51209e7912bb0f2165e28099453c
byunyoosuk <qusdbtjr91@dbinc.co.kr>
2025-08-08 17:19:58 +0900

    엑셀 파일의 DB 반영 예외처리 로직 수정

--- src/main/java/com/ito/collector/service/ExcelAssetUpdaterService.java
+++ src/main/java/com/ito/collector/service/ExcelAssetUpdaterService.java
@@ -8,6 +8,7 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,14 +19,16 @@ import java.util.Optional;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-
 public class ExcelAssetUpdaterService {
 
     private final CmdbAssetRepository assetRepository;
+    private final WikiUploadService wikiUploadService;  // WikiUploadService 추가
 
     // 엑셀 파일 경로 (수정 가능)
+    //private static final String EXCEL_PATH = "C:\\Users\\Administrator\\Desktop\\project\\wiki\\DBWikiAgent\\src\\main\\resources\\server_linux.xlsx";
     private static final String EXCEL_PATH = "src/main/resources/server_linux.xlsx";
 
+    @Transactional // 트랜잭션을 명시적으로 관리
     public void updateAssetsFromExcel() {
         try {
             File excelFile = new File(EXCEL_PATH);
@@ -53,6 +56,7 @@ public class ExcelAssetUpdaterService {
                     String workType   = getCellValue(row, 13).trim();  // 공백 제거
                     String osManager  = getCellValue(row, 20);
                     String mwManager  = getCellValue(row, 21);
+                    String workCategory = getCellValue(row, 11);
 
                     // workType이 "DR"이면 해당 항목을 건너뜁니다.
                     if ("DR".equalsIgnoreCase(workType)) {  // 대소문자 구분 없이 비교
@@ -73,6 +77,7 @@ public class ExcelAssetUpdaterService {
                             existingAsset.setMem(mem);
                             existingAsset.setOsManager(osManager);
                             existingAsset.setMwManager(mwManager);
+                            existingAsset.setWorkCategory(workCategory);
                         }
                     } else {
                         CmdbAsset newAsset = new CmdbAsset();
@@ -84,18 +89,39 @@ public class ExcelAssetUpdaterService {
                         newAsset.setWorkType(workType);
                         newAsset.setOsManager(osManager);
                         newAsset.setMwManager(mwManager);
+                        newAsset.setWorkCategory(workCategory);
 
                         hostnameToAssetMap.put(hostname, newAsset);
                     }
                 }
 
+                // **"DR" 항목 삭제**: DR 항목을 DB에서 삭제
+                for (CmdbAsset asset : hostnameToAssetMap.values()) {
+                    if ("DR".equalsIgnoreCase(asset.getWorkType())) {
+                        assetRepository.delete(asset);  // DB에서 삭제
+                        log.info("삭제된 자산: {}", asset.getHostname());
+                    }
+                }
+
                 // 저장된 자산 정보를 DB에 저장
                 for (CmdbAsset asset : hostnameToAssetMap.values()) {
-                    assetRepository.save(asset);
-                    log.info("자산 업데이트됨: {}", asset.getHostname());
+                    // DR이 아닌 자산만 저장
+                    if (!"DR".equalsIgnoreCase(asset.getWorkType())) {
+                        assetRepository.save(asset);
+                        assetRepository.flush();  // 즉시 DB에 반영
+                        log.info("자산 업데이트됨: {}", asset.getHostname());
+                    }
                 }
 
                 log.info("엑셀 기반 자산 업데이트 완료");
+
+                // DB에 업데이트가 완료된 후, 해당 자산들의 위키 페이지도 업데이트
+                for (CmdbAsset asset : hostnameToAssetMap.values()) {
+                    if (!"DR".equalsIgnoreCase(asset.getWorkType())) {
+                        wikiUploadService.uploadPage(asset.getHostname());  // 위키에 페이지 업로드
+                        log.info("위키 페이지 업로드됨: {}", asset.getHostname());
+                    }
+                }
 
             }
 
@@ -104,6 +130,7 @@ public class ExcelAssetUpdaterService {
         }
     }
 
+    // 셀 값 가져오기 (null 처리 및 공백 제거)
     private String getCellValue(Row row, int colIdx) {
         try {
             Cell cell = row.getCell(colIdx);
